7edf573a857c880d029c7f0ef38c722be3774e63
Leon_lp9 <73760817+Leonlp9@users.noreply.github.com>
2024-11-21 22:51:11 +0100

    Refactor UntisAPIPlugin to use server list element and enhance button styling

--- src/plugins/untisApi/index.tsx
+++ src/plugins/untisApi/index.tsx
@@ -6,11 +6,12 @@
 
 import "./style.css";
 
-import { addChatBarButton, ChatBarButton, removeChatBarButton } from "@api/ChatButtons";
+import { addServerListElement, removeServerListElement, ServerListRenderPosition } from "@api/ServerList";
 import { definePluginSettings } from "@api/Settings";
+import ErrorBoundary from "@components/ErrorBoundary";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { FluxDispatcher, React, useState } from "@webpack/common";
+import { Button, FluxDispatcher, React } from "@webpack/common";
 
 import WebUntisAPI from "./api/untisApi";
 
@@ -163,88 +164,75 @@ const settings = definePluginSettings({
 
 function onChange() {
 }
+const handleButtonClick = async () => {
 
-const UntisAPIPlugin: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleButtonClick = async () => {
-
-        const webUntis = new WebUntisAPI(
-            settings.store.School || "defaultSchool",
-            settings.store.UntisUsername || "defaultUsername",
-            settings.store.Key || "defaultKey",
-            settings.store.Untisver || "defaultVersion",
-            settings.store.UntisType || "defaultType"
-        );
-        await webUntis.setUp();
-        setIsOpen(!isOpen);
-
-
-
-        try {
-            // Authenticate the WebUntisAPI instance
-
-            console.log("Fetching timetable...");
-            const currentlessen = await webUntis.getCurrentLesson(1);
-            if (!currentlessen) {
-                console.log("No lesson found");
-
-            } else {
-                FluxDispatcher.dispatch({
-                    type: "LOCAL_ACTIVITY_UPDATE",
-                    activity: {
-                        application_id: "",
-                        flags: 1,
-                        name: "Untericht",
-                        details: "Grad in einer stunde",
-                        type: 0,
-
-                    },
-                    socketId: "CustomRPC",
-                });
-
-
-                // set rpc
-
-
-            }
-        } catch (error) {
-            console.error("Error fetching timetable:", error);
-        }
-    };
-
-
-
-    return (
-        <ChatBarButton
-            tooltip="Enable UntisRPC"
-            onClick={handleButtonClick}
-        >
-            <svg className="untis-button" viewBox="0 0 24 24" fill="currentColor">
-                <path fill="currentColor" className="st0" d="M12,0C5.37,0,0,5.37,0,12s5.37,12,12,12,12-5.37,12-12S18.63,0,12,0ZM1.53,12.33v-.67h3.89v.67H1.53ZM4.83,19.64l-.47-.47,2.75-2.75.47.47-2.75,2.75ZM7.11,7.58l-2.75-2.75.47-.47,2.75,2.75-.47.47ZM12.33,22.47h-.67v-3.89h.67v3.89ZM19.17,19.64l-2.75-2.75.47-.47,2.75,2.75-.47.47ZM11,12V2.53c.39-.39.61-.61,1-1,.39.39.61.61,1,1v6.05l4.8-4.8h1.42v1.42l-4.8,4.8h7.05c.39.39.61.61,1,1-.39.39-.61.61-1,1h-10.47Z" />
-            </svg>
-
-
-
-        </ChatBarButton>
-
+    const webUntis = new WebUntisAPI(
+        settings.store.School || "defaultSchool",
+        settings.store.UntisUsername || "defaultUsername",
+        settings.store.Key || "defaultKey",
+        settings.store.Untisver || "defaultVersion",
+        settings.store.UntisType || "defaultType"
     );
+    await webUntis.setUp();
+
+    try {
+        // Authenticate the WebUntisAPI instance
+
+        console.log("Fetching timetable...");
+        const currentlessen = await webUntis.getCurrentLesson(1);
+        if (!currentlessen) {
+            console.log("No lesson found");
+
+        } else {
+            FluxDispatcher.dispatch({
+                type: "LOCAL_ACTIVITY_UPDATE",
+                activity: {
+                    application_id: "",
+                    flags: 1,
+                    name: "Untericht",
+                    details: "Grad in einer stunde",
+                    type: 0,
+
+                },
+                socketId: "CustomRPC",
+            });
 
 
+            // set rpc
 
+
+        }
+    } catch (error) {
+        console.error("Error fetching timetable:", error);
+    }
 };
+
+const UntisButton = () => (
+    <Button
+        onClick={handleButtonClick}
+        size={Button.Sizes.MIN}
+        color={Button.Colors.CUSTOM}
+        className="vc-untis-button"
+    >
+        <svg className="untis-button" viewBox="0 0 24 24" fill="currentColor">
+            <path fill="currentColor" className="st0" d="M12,0C5.37,0,0,5.37,0,12s5.37,12,12,12,12-5.37,12-12S18.63,0,12,0ZM1.53,12.33v-.67h3.89v.67H1.53ZM4.83,19.64l-.47-.47,2.75-2.75.47.47-2.75,2.75ZM7.11,7.58l-2.75-2.75.47-.47,2.75,2.75-.47.47ZM12.33,22.47h-.67v-3.89h.67v3.89ZM19.17,19.64l-2.75-2.75.47-.47,2.75,2.75-.47.47ZM11,12V2.53c.39-.39.61-.61,1-1,.39.39.61.61,1,1v6.05l4.8-4.8h1.42v1.42l-4.8,4.8h7.05c.39.39.61.61,1,1-.39.39-.61.61-1,1h-10.47Z" />
+        </svg>
+    </Button>
+);
 
 export default definePlugin({
     name: "UntisAPI",
     description: "Show your current lesson in Discord",
     authors: [Devs.Leonlp9, Devs.minikomo],
     settings,
-    component: UntisAPIPlugin, // Use the UntisAPIPlugin component
-    async start() {
-        addChatBarButton("UntisAPI", () => <UntisAPIPlugin />); // Add a chat bar button to open the UntisAPIPlugin component
 
+    renderUntisButton: ErrorBoundary.wrap(UntisButton, { noop: true }),
+
+    start() {
+        addServerListElement(ServerListRenderPosition.Above, this.renderUntisButton);
     },
+
     stop() {
-        removeChatBarButton("UntisAPI"); // Remove the chat bar button when the plugin is stopped
+        removeServerListElement(ServerListRenderPosition.Above, this.renderUntisButton);
     }
 });
