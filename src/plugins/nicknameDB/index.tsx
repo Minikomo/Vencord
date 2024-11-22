3e80da46930e7a3f34f753862413a1fa25281536
Minikomo <102686307+Minikomo@users.noreply.github.com>
2024-11-22 22:19:47 +0100

    Update index.tsx

---
+++ src/plugins/nicknameDB/index.tsx
@@ -0,0 +1,24 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+
+definePlugin({
+
+    name: "NicknameDB",
+    description: "Lets you globally set nicknames for users and vote for the best ones.",
+    authors: [Devs.minikomo],
+
+
+    start() {
+
+
+
+    }
+
+});
