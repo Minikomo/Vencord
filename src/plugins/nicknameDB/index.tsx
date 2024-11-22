b2d45fa9864246f613493655992c47c7c0541c62
Minikomo <102686307+Minikomo@users.noreply.github.com>
2024-11-22 22:30:47 +0100

    Update index.tsx

--- src/plugins/nicknameDB/index.tsx
+++ src/plugins/nicknameDB/index.tsx
@@ -7,8 +7,30 @@
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
+const guildPopoutPatch: NavContextMenuPatchCallback = (children, { guild }: { guild: Guild, onClose(): void; }) => {
+    if (!guild) return;
+    children.push(
+        <Menu.MenuItem
+            label="View Reviews"
+            id="vc-rdb-server-reviews"
+            icon={OpenExternalIcon}
+            action={() => openReviewsModal(guild.id, guild.name, ReviewType.Server)}
+        />
+    );
+};
+
+
 export default definePlugin({
     name: "name",
     description: "description",
-    authors: [Devs.author],
+    authors: [Devs.minikomo],
+
+
+
+    start() {
+
+    },
+
+
 });
+
