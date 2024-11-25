429c81b77c575a9a4a6cbaec7c0c1a8b42bd5377
Leon_lp9 <73760817+Leonlp9@users.noreply.github.com>
2024-11-25 10:12:40 +0100

    IconViewer: Add a new settings tab for icon preview and management

---
+++ src/plugins/iconViewer/index.tsx
@@ -0,0 +1,41 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { SettingsRouter } from "@webpack/common";
+
+// import settings from "../_core/settings";
+import IconsTab from "./IconsTab";
+
+export default definePlugin({
+    name: "IconViewer",
+    description: "Adds a new tab to settings, to preview all icons",
+    authors: [Devs.Leonlp9],
+    dependencies: ["Settings"],
+    toolboxActions: {
+        "Open Icons Tab"() {
+            SettingsRouter.open("VencordDiscordIcons");
+        },
+    },
+    insertSettings() {
+        return {
+            section: "VencordDiscordIcons",
+            label: "Icons",
+            element: IconsTab,
+            className: "vc-discord-icons",
+        };
+    },
+    start() {
+        // @ts-ignore
+        Vencord.Plugins.plugins.Settings.customSections.push(this.insertSettings);
+    },
+    stop() {
+        // @ts-ignore
+        const { customSections } = Vencord.Plugins.plugins.Settings;
+        customSections.splice(customSections.indexOf(this.insertSettings), 1);
+    },
+});
